23994fe35572b33ea77de26a4ab3e234fd33cc9a
Jason Mansfield <jason@lub-dub.org>
2025-07-15 14:22:14 -0700

    remove banter dep

--- cmd/ak/main.go
+++ cmd/ak/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/autonomouskoi/akcore/exe"
-	_ "github.com/autonomouskoi/banter"
 	_ "github.com/autonomouskoi/trackstar"
 	_ "github.com/autonomouskoi/trackstar/mixxx"
 	_ "github.com/autonomouskoi/trackstar/stagelinq"
