e85aef9011ecac4c734fb0d0552e4b166c4d758e
Jason Mansfield <jason@lub-dub.org>
2025-08-07 16:04:44 -0700

    include trackstar/prodjlink

--- cmd/ak/main.go
+++ cmd/ak/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/autonomouskoi/akcore/exe"
 	_ "github.com/autonomouskoi/trackstar"
 	_ "github.com/autonomouskoi/trackstar/mixxx"
+	_ "github.com/autonomouskoi/trackstar/prodjlink"
 	_ "github.com/autonomouskoi/trackstar/stagelinq"
 	_ "github.com/autonomouskoi/twitch"
 )
